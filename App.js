f094f43a1c14c0115c1c4ea08ce470514996766b
M-A-C255 <zaypainglatt04@gmail.com>
2025-07-29 18:20:00 +0800

    Added home screen navigation

--- App.js
+++ App.js
@@ -1,27 +1,14 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-const Stack = createNativeStackNavigator();
-
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Receipt App!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet } from "react-native";
+import AppNavigator from "./src/components/AppNavigator";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar style="auto" />
+      <AppNavigator />
+    </>
   );
 }
 
