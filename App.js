79c53cca4de00925a75925992820f432ca193e0f
M-A-C255 <zaypainglatt04@gmail.com>
2025-08-06 12:50:31 +0800

    wrapped app.js with authprovider

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import AppNavigator from "./src/components/AppNavigator";
+import { AuthProvider } from "./src/context/AuthContext";
 
 export default function App() {
   return (
-    <>
+    <AuthProvider>
       <StatusBar style="auto" />
       <AppNavigator />
-    </>
+    </AuthProvider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
