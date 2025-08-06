027d908d03981af7092d6423ae626ef5b37e12aa
Mac <zaypainglatt04@gmail.com>
2025-08-06 20:14:33 +0800

    UI/color-changes (#2)

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,16 +1,19 @@
 // styles/styles.js
 import { StyleSheet } from "react-native";
 
+const navyBlue = "#0c2d5d";
+const lightCream = "#f2efe7";
+const darkPink = "#781d4e";
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     padding: 24,
-    backgroundColor: "#f5f5f5",
+    backgroundColor: navyBlue,
   },
   input: {
     height: 50,
-    borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 10,
     paddingHorizontal: 15,
@@ -18,21 +21,41 @@ export const styles = StyleSheet.create({
     marginBottom: 16,
   },
   title: {
-    fontSize: 28,
+    fontSize: 40,
+    fontFamily: "Montserrat",
     fontWeight: "bold",
     textAlign: "center",
-    marginBottom: 24,
+    color: lightCream,
+    marginBottom: 30,
+  },
+  subtitle: {
+    fontSize: 30,
+    textAlign: "center",
+    color: lightCream,
+    fontFamily: "Montserrat",
+    marginBottom: 16,
   },
   button: {
-    backgroundColor: "#1e90ff",
+    backgroundColor: lightCream,
+    borderWidth: 1,
     paddingVertical: 14,
     borderRadius: 10,
     alignItems: "center",
     marginVertical: 10,
   },
   buttonText: {
-    color: "#fff",
+    color: lightCream,
     fontSize: 16,
     fontWeight: "bold",
+  },
+  linkText: {
+    marginTop: 14,
+    fontSize: 14,
+    color: lightCream,
+    textAlign: "center",
+  },
+  linkTextHighlight: {
+    color: darkPink,
+    fontWeight: "600",
   },
 });
