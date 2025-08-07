6899d59439bb3a1ab88df3081b39749b625d4d7f
M-A-C255 <zaypainglatt04@gmail.com>
2025-08-07 19:44:23 +0800

    added 4 icons to nav bar

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -3,8 +3,10 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/Home";
 import ProfileScreen from "../screens/Profile";
-import SettingsScreen from "../screens/Settings";
+import GroceryListScreen from "../screens/GroceryList";
+import CameraScreen from "../screens/Camera";
 import { Ionicons } from "@expo/vector-icons";
+import { styles, navyBlue } from "../styles/styles";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,21 +20,27 @@ export default function BottomTabNavigator() {
 
           if (route.name === "Home") {
             iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Camera") {
+            iconName = focused ? "camera" : "camera-outline";
+          } else if (route.name === "Grocery") {
+            iconName = focused ? "cart" : "cart-outline";
           } else if (route.name === "Profile") {
             iconName = focused ? "person" : "person-outline";
-          } else if (route.name === "Settings") {
-            iconName = focused ? "settings" : "settings-outline";
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: "#2e64e5",
+        tabBarActiveTintColor: navyBlue,
         tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          ...styles.navBar,
+        },
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Grocery" component={GroceryListScreen} />
+      <Tab.Screen name="Camera" component={CameraScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
 }
