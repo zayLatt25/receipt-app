fabe25fe2d54c362f0c077e4ee81b249982c442f
Mac <zaypainglatt04@gmail.com>
2025-08-08 20:00:49 +0800

    feature/logout (#9)

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,28 @@
 // src/screens/Profile.js
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
+import { styles } from "../styles/styles";
+import FormButton from "../components/FormButton";
+import { auth } from "../firebase";
 
 export default function ProfileScreen() {
+  const [logoutLoading, setLogoutLoading] = useState(false);
+
+  const handleLogout = () => {
+    setLogoutLoading(true);
+    auth.signOut();
+    setLogoutLoading(false);
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Profile Screen</Text>
+    <View style={styles.centerContainer}>
+      <Text style={styles.title}>Profile Screen</Text>
+
+      <FormButton
+        title="Logout"
+        onPress={handleLogout}
+        loading={logoutLoading}
+      />
     </View>
   );
 }
