da712611cd97fc0bd364a1a30aa54549254cce0f
M-A-C255 <zaypainglatt04@gmail.com>
2025-08-10 02:37:01 +0800

    init grocery list page

--- src/screens/GroceryList.js
+++ src/screens/GroceryList.js
@@ -1,11 +1,12 @@
 // src/screens/GroceryList.js
 import React from "react";
 import { View, Text } from "react-native";
+import { styles } from "../styles/styles";
 
 export default function GroceryListScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Grocery List Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.subtitle}>Grocery List Screen</Text>
     </View>
   );
 }
