f0d8a5bb0691f34e0da96b9ec8c25267a0540a19
M-A-C255 <zaypainglatt04@gmail.com>
2025-08-15 02:36:17 +0800

    changed form button

--- src/components/FormButton.js
+++ src/components/FormButton.js
@@ -3,13 +3,9 @@ import React from "react";
 import { TouchableOpacity, Text, ActivityIndicator } from "react-native";
 import { styles } from "../styles/styles";
 
-const FormButton = ({ title, onPress, loading = false }) => {
+const FormButton = ({ title, onPress, loading = false, style }) => {
   return (
-    <TouchableOpacity
-      style={styles.button}
-      onPress={onPress}
-      disabled={loading}
-    >
+    <TouchableOpacity onPress={onPress} disabled={loading} style={style}>
       {loading ? (
         <ActivityIndicator size="small" color="#fff" />
       ) : (
