45baf7d152cb0335d9d606b9e121070c0a753884
M-A-C255 <zaypainglatt04@gmail.com>
2025-08-21 04:50:04 +0800

    Update ProfileSettings.js

--- src/components/ProfileSettings.js
+++ src/components/ProfileSettings.js
@@ -5,7 +5,7 @@ import { styles } from "../styles/styles";
 export default function ProfileSettings() {
   return (
     <View>
-      <Text style={styles.profileText}>Settings content goes here.</Text>
+      <Text style={styles.profileText}>Settings coming soon</Text>
       {/* TODO: Add more settings content here */}
     </View>
   );
